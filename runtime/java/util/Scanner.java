7d65463e982f5ce98385aee25ed00aa1be40382f
xb-bx <nazarnik050406@gmail.com>
2023-09-05 13:49:58 +0300

    basic scanner and StringBuidler

--- runtime/java/util/Scanner.java
+++ runtime/java/util/Scanner.java
@@ -3,10 +3,17 @@ import java.io.InputStream;
 import java.lang.NotImplementedException;
 
 public class Scanner {
+    private final InputStream stream;
     public Scanner(InputStream stream) {
-        throw new NotImplementedException();
+        this.stream = stream;
     }
     public String nextLine() {
-        throw new NotImplementedException();
+        StringBuilder sb = new StringBuilder();
+        char r = 0;
+        do {
+            if(r != 0) sb.append(r);
+            r = (char)stream.read();
+        } while(r != '\n');
+        return sb.toString();
     }
 }
